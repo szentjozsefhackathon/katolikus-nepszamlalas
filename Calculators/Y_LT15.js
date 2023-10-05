379f32e48928cb619d6715843d0b4555ff902d5b
vlacko0930 <theprogrambear@gmail.com>
2023-10-05 22:49:18 +0200

    Add checkboxes to select dioceses

--- Calculators/Y_LT15.js
+++ Calculators/Y_LT15.js
@@ -1,8 +1,6 @@
 export function generate(fullData, colls) {
-    console.log("Calc3")
     colls.push('Y_LT15');
     $.each(fullData, function (key, value) {
-        console.log(value.data);
         fullData[key]['data']['Y_LT15'] = {
             '2001': value.data['FY_LT15'][2001] + value.data['FY_LT15'][2001],
             '2011': value.data['FY_LT15'][2011] + value.data['FY_LT15'][2011],
@@ -10,6 +8,5 @@ export function generate(fullData, colls) {
         };
 
     });
-    console.log(fullData);
     return [fullData, colls];
 };
