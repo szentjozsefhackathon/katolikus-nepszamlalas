bd04d85d5815b0c973e47e436b29cb3ed3dc5da4
vlacko0930 <theprogrambear@gmail.com>
2023-10-19 16:30:02 +0200

    Add nodes to config

--- addNode.js
+++ addNode.js
@@ -1,4 +1,4 @@
-if ($("#addNodeContainer").length==1) {
+if ($("#addNodeContainer").length == 1) {
   $("#addNodeContainer").append(`
     <div class="container">
       <label for="newNodeType">Megjelenítés típusa</label>
@@ -31,29 +31,60 @@ if ($("#addNodeContainer").length==1) {
         </div>
       </div>
       <div id="container_NewNodeInProprotionTo">
-        <label for="newNodeInProprotionTo">Arányosítva ehhez</label>
-        <select class="form-control" id="newNodeInProprotionTo"></select>
+        <input class="form-check-input" type="checkbox" id="newNodeInProprotionToCheck" checked>
+        <label class="form-check-label" for="newNodeInProprotionToCheck">
+          Arányosítás
+        </label>
+        <br>
+        <div id="container_newNodeInProprotionTo_Field">
+          <label for="newNodeInProprotionTo">Arányosítva ehhez</label>
+          <select class="form-control" id="newNodeInProprotionTo"></select>
+        </div>
       </div>
+      <button id="addNodeButton" type="button" class="btn btn-primary">Elem hozzáadása</button>
+
    </div>`
   );
 }
 
-
+$("#newNodeInProprotionToCheck").click(function() {
+  if ($("#newNodeInProprotionToCheck").prop("checked")) $("#container_newNodeInProprotionTo_Field").show()
+  else $("#container_newNodeInProprotionTo_Field").hide()
+})
 
 $("#newNodeType").on("change", event => {
   const mu = Markup.list()[$("#newNodeType").val()]
-  if(mu.year) $("#newNodeYears").show()
+  if (mu.year) $("#newNodeYears").show()
   else $("#newNodeYears").hide()
 
-  if(mu.inProprotionTo) $("#container_NewNodeInProprotionTo").show()
+  if (mu.inProprotionTo) $("#container_NewNodeInProprotionTo").show()
   else $("#container_NewNodeInProprotionTo").hide()
 
-  if(mu.multipleData)  $("#newNodeData").attr("multiple", "multiple")
+  if (mu.multipleData) $("#newNodeData").attr("multiple", "multiple")
   else $("#newNodeData").removeAttr("multiple")
 
   $("#newNodeData").select2()
 
-
 })
 
 $("#newNodeType").trigger("change")
+
+
+function getNewNodeData() {
+  const node = {markup: $("#newNodeType").val(), data: $("#newNodeData").val()}
+  const mu = Markup.list()[$("#newNodeType").val()]
+  if (mu.year) {
+    node.years = YEARS.filter(y => $(`#newNode${y}`).prop("checked"))
+  }
+  if (mu.inProprotionTo && $("#newNodeInProprotionToCheck").prop("checked")) {
+    node.inProprotionTo = $("#newNodeInProprotionTo").val()
+  }
+
+  return node
+}
+
+$("#addNodeButton").click(function() {
+  settings.push(getNewNodeData())
+  changeSetting()
+  publish()
+})
